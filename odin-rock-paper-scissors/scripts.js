1cadee62fd57bd4fbbccfb7f0c1f33057808c21e
noahchen709 <119009420+noahchen709@users.noreply.github.com>
2024-12-20 21:30:44 -0500

    feat: Generate a choice for computer

--- odin-rock-paper-scissors/scripts.js
+++ odin-rock-paper-scissors/scripts.js
@@ -1,1 +1,14 @@
+function getComputerChoice() {
+    const numToChoice = {
+        1: "rock",
+        2: "paper",
+        3: "scissors"
+    };
 
+    // 3 is the number of choices
+    randNumToThree = Math.ceil(Math.random() * 3);
+    choice = numToChoice[randNumToThree];
+    return choice;
+}
+
+console.log(getComputerChoice());
