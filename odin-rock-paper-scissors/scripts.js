1027ed37245a5246a2fdc8c11f594543d16a1270
noahchen709 <119009420+noahchen709@users.noreply.github.com>
2024-12-21 11:58:35 -0500

    chore: Same as before

--- odin-rock-paper-scissors/scripts.js
+++ odin-rock-paper-scissors/scripts.js
@@ -1,12 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const numToChoice = {
-        1: "rock",
-        2: "paper",
-        3: "scissors"
-    };
-
     // 3 is the number of choices
     let randNumToThree = Math.floor(Math.random() * 3);
     let choice = choices[randNumToThree];
