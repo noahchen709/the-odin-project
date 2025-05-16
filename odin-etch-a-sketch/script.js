62ed0734162c5892afbc2eaa6a3c7d9729a9dd09
noahchen709 <119009420+noahchen709@users.noreply.github.com>
2025-05-16 16:44:44 +0800

    use a variable to store squares per side

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
+const squaresPerSide = 4;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < Math.pow(squaresPerSide, 2); i++) {
     const newSquare = document.createElement("div");
     newSquare.setAttribute("id", "square" + i);
     container.appendChild(newSquare);
