693a4f67fefefa7cea63ea8c455d0b01c7873aa9
noahchen709 <119009420+noahchen709@users.noreply.github.com>
2025-05-16 17:43:31 +0800

    change squares color on hover

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector("#container");
-const squaresPerSide = 4;
+const squaresPerSide = 16;
 const totalSquares = Math.pow(squaresPerSide, 2);
 
 container.style.setProperty('--squares-per-side', squaresPerSide);
@@ -8,6 +8,9 @@ for (let i = 0; i < totalSquares; i++) {
     const newSquare = document.createElement("div");
     newSquare.setAttribute("id", "square" + i);
     newSquare.classList.add("newSquare");
+    newSquare.addEventListener("mouseover", () => {
+        newSquare.classList.add("penColor");
+    });
 
     container.appendChild(newSquare);
 }
