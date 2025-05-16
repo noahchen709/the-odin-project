b45f7277981d95c65480628850941adb8564ec8c
noahchen709 <119009420+noahchen709@users.noreply.github.com>
2025-05-16 17:07:18 +0800

    position the squares in grid

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector("#container");
 const squaresPerSide = 4;
+const totalSquares = Math.pow(squaresPerSide, 2);
 
-for (let i = 0; i < Math.pow(squaresPerSide, 2); i++) {
+container.style.setProperty('--squares-per-side', squaresPerSide);
+
+for (let i = 0; i < totalSquares; i++) {
     const newSquare = document.createElement("div");
     newSquare.setAttribute("id", "square" + i);
+    newSquare.classList.add("newSquare");
+
     container.appendChild(newSquare);
 }
